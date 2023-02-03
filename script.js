de568430bb978f4bd63ae29d80c8869bd21e2c35
EdmondNeziraj <edmondneziraj10@gmail.com>
2023-02-03 11:59:24 -0500

    added styling for the popup after the game ends

--- script.js
+++ script.js
@@ -15,12 +15,10 @@ let finalResult_p = document.getElementById("final-result");
 
 
 function computerPlay() {
-    // get a random number
-    let index = Math.floor(Math.random() * 100) % 3;
     const choices = ["Rock", "Paper", "Scissors"];
 
-    console.log(index);
-    console.log(choices[index]);
+    // get a random number
+    let index = Math.floor(Math.random() * 100) % 3;
 
     // return one of the three options 
     return choices[index];
@@ -34,65 +32,65 @@ function playRound(playerSelection, computerSelection) {
     // compare the value of the player with the value of the computer
     if (playerSelection == "Rock") {
         if (computerSelection == "Rock") {
-            result_p.innerHTML = "Both chose rock. It's a draw!";
+            result_p.innerHTML = "Both chose rock. It's a draw this time";
             rock_div.style.borderColor = "rgb(165, 165, 165)";
-            setTimeout(function () { rock_div.style.borderColor = "whitesmoke" }, 500);
+            setTimeout(function () { rock_div.style.borderColor = "#fff" }, 500);
         }
         else if (computerSelection == "Paper") {
             computerScore += 1;
             computerScore_span.innerHTML = computerScore;
-            result_p.innerHTML = "Paper beats rock. You lose!";
+            result_p.innerHTML = "Paper beats rock. You lost this round";
             rock_div.style.borderColor = "red";
-            setTimeout(function () { rock_div.style.borderColor = "whitesmoke" }, 500);
+            setTimeout(function () { rock_div.style.borderColor = "#fff" }, 500);
         }
         else {
             playerScore += 1;
             playerScore_span.innerHTML = playerScore;
-            result_p.innerHTML = "Rock beats scissors. You win!";
+            result_p.innerHTML = "Rock beats scissors. You won this round!";
             rock_div.style.borderColor = "green";
-            setTimeout(function () { rock_div.style.borderColor = "whitesmoke" }, 500);
+            setTimeout(function () { rock_div.style.borderColor = "#fff" }, 500);
         } // end if ... else
     }
     else if (playerSelection == "Paper") {
         if (computerSelection == "Rock") {
             playerScore += 1;
             playerScore_span.innerHTML = playerScore;
-            result_p.innerHTML = "Paper beats rock. You win!";
+            result_p.innerHTML = "Paper beats rock. You won this round!";
             paper_div.style.borderColor = "green";
-            setTimeout(function () { paper_div.style.borderColor = "whitesmoke" }, 500);
+            setTimeout(function () { paper_div.style.borderColor = "#fff" }, 500);
         }
         else if (computerSelection == "Paper") {
-            result_p.innerHTML = "Both chose paper. It's a draw!";
+            result_p.innerHTML = "Both chose paper. It's a draw this time";
             paper_div.style.borderColor = "rgb(165, 165, 165)";
-            setTimeout(function () { paper_div.style.borderColor = "whitesmoke" }, 500);
+            setTimeout(function () { paper_div.style.borderColor = "#fff" }, 500);
         }
         else {
             computerScore += 1;
             computerScore_span.innerHTML = computerScore;
-            result_p.innerHTML = "Scissors beat paper. You lose!";
+            result_p.innerHTML = "Scissors beat paper. You lost this round";
             paper_div.style.borderColor = "red";
-            setTimeout(function () { paper_div.style.borderColor = "whitesmoke" }, 500);
+            setTimeout(function () { paper_div.style.borderColor = "#fff" }, 500);
         } // end if ... else
     }
     else { // player chooses scissors
         if (computerSelection == "Rock") {
             computerScore += 1;
             computerScore_span.innerHTML = computerScore;
-            result_p.innerHTML = "Rock beats scissors. You lose!";
+            result_p.innerHTML = "Rock beats scissors. You lost this round";
             scissors_div.style.borderColor = "red";
-            setTimeout(function () { scissors_div.style.borderColor = "whitesmoke" }, 500);
+            setTimeout(function () { scissors_div.style.borderColor = "#fff" }, 500);
         }
         else if (computerSelection == "Paper") {
             playerScore += 1;
             playerScore_span.innerHTML = playerScore;
-            result_p.innerHTML = "Scissors beat paper. You win!";
+            result_p.innerHTML = "Scissors beat paper. You won this round";
             scissors_div.style.borderColor = "green";
-            setTimeout(function () { scissors_div.style.borderColor = "whitesmoke" }, 500);
+            setTimeout(function () { scissors_div.style.borderColor = "#fff" }, 500);
         }
         else {
-            result_p.innerHTML = "Both chose scissors. It's a draw!";
+            result_p.innerHTML = "Both chose scissors. It's a draw this time";
             scissors_div.style.borderColor = "rgb(165, 165, 165)";
-            setTimeout(function () { scissors_div.style.borderColor = "whitesmoke" }, 500);
+            setTimeout(function () { scissors_div.style.borderColor = "#fff" }, 500);
         } // end if ... else
     } // end if ... else
 } // end playRound
@@ -103,32 +101,23 @@ function playRound(playerSelection, computerSelection) {
 function game(playerSelection) {
     const computerSelection = computerPlay();
 
-
-    console.log(playRound(playerSelection, computerSelection));
-    console.log(`Player: ${playerScore}, Computer: ${computerScore}`)
-    console.log(`Player chose: ${playerSelection}, Computer chose: ${computerSelection}`);
+    playRound(playerSelection, computerSelection);
 
     if (playerScore == 5 || computerScore == 5) {
         // figure out the winner and keep track of score
         if (playerScore > computerScore) {
-            // alert("You won! To replay press Okay")
-            // announce here the winner with a pop up and add button to restart the game
             announceWinner("You won! Congratulations!")
         }
         else if (playerScore < computerScore) {
             finalResult_p.innerHTML = "You lost!";
-            // alert("You lost! To replay press Okay")
             announceWinner("You lost :( Try again!")
         }
-        // reset();
     }
 }
 
 
 // announce winner at the end of the game
 function announceWinner(text) {
-    
-
     const popup = document.querySelector('.popup');
     const resetButton = document.querySelector('.reset');
     popup.classList.add('active');
@@ -140,9 +129,6 @@ function announceWinner(text) {
         reset();
     });
 }
-
-
-
 
 // reset game 
 function reset() {
